9028f38505ca9322630c14f1f9515e6fa478dd14
Max Jung <max@gourmetjs.com>
2019-11-13 16:55:18 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import net from "net";
+import os from "os";
 import express from "express";
 import morgan from "morgan";
+
+const content = `
+<h1>Hello, world!</h1>
+<pre>
+hostname: ${os.hostname()}
+</pre>
+`;
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello, world!</h1>");
+  res.send(content);
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(4444, () => {
   console.log(`Server is listening on port ${(server.address() as net.AddressInfo).port}`);
 });
