7e766390f0f0a6b1b8b3c691490c444bf3dfe0ec
Max Jung <max@gourmetjs.com>
2019-11-15 10:31:56 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import net from "net";
 import os from "os";
+import fs from "fs";
 import express from "express";
 import morgan from "morgan";
 
@@ -9,6 +10,17 @@ const content = `
 hostname: ${os.hostname()}
 </pre>
 `;
+
+function cat(path: string): string {
+  try {
+    const content = fs.readFileSync(path, "utf8");
+    return content;
+  } catch (err) {
+    if (err.code != "ENOENT")
+      throw err;
+    return "";
+  }
+}
 
 const app = express();
 
@@ -22,4 +34,8 @@ const server = app.listen(4444, () => {
   console.log(`Server is listening on port ${(server.address() as net.AddressInfo).port}`);
 });
 
-console.log(`Server started on the hostname: ${os.hostname()}`);
+console.log(`Server is running on the following environment:`);
+console.log(`  hostname: ${os.hostname()}`);
+console.log(`  node: ${process.version}`);
+console.log(`  /proc/version: ${cat("/proc/version")}`);
+console.log(`  /etc/issue: ${cat("/etc/issue")}`);
